05343dea106b8ae1da4e40a686284d5f250f6795
taras-rm <tarasrom25@gmail.com>
2026-02-07 12:14:41 -0500

    users db change

--- db/queries/query.sql
+++
@@ -1,4 +0,0 @@
--- name: GetUserByEmail :one
-SELECT *
-FROM users
-WHERE email = $1;
---
+++ db/queries/users.sql
@@ -0,0 +1,9 @@
+-- name: GetUserByEmail :one
+SELECT *
+FROM users
+WHERE email = $1;
+
+-- name: GetUserById :one
+SELECT *
+FROM users
+WHERE id = $1;
